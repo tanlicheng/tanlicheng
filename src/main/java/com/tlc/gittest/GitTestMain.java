8f604f5c20536243ff6e99e6ce9040bed7d5917e
tlc <tlc@tlc.com>
2024-11-30 15:39:25 +0800

    hot fix commit

--- src/main/java/com/tlc/gittest/GitTestMain.java
+++ src/main/java/com/tlc/gittest/GitTestMain.java
@@ -3,5 +3,6 @@ package com.tlc.gittest;
 public class GitTestMain {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git hot fix");
     }
 }
